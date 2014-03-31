a94c2457c5cbcd6c635185230afef77bd8b4d9a9
mishu-mishu <elena.posea@gmail.com>
2014-03-31 19:25:32 +0300

    formularul de inregistrare, fara code behind #3

--- ProiectIP2014/Inregistrare.aspx.cs
+++ ProiectIP2014/Inregistrare.aspx.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class SignUp : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
+public partial class SignUp : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
 
     }
+    protected void CreateUserWizard1_ContinueButtonClick(object sender, EventArgs e) {
+        //Debug.WriteLine("continue button");
+        //try {
+        //Profile.RegisterDate = System.DateTime.Now;
+        //Response.Redirect("~/Default.aspx", false);
+        // }
+        //catch (Exception exceptie) {
+        //   Debug.WriteLine("continue button am prins exceptia "+ exceptie);        
+        //}
+        //   Debug.WriteLine("continue button after redirect");
+    }
+
+    protected void CreateUserWizard1_CreatedUser(object sender, EventArgs e) {
+        // aici ii spun ca vreau ca by default userul sa fie ONG
+        Debug.WriteLine("create user");
+        try {
+            Roles.AddUserToRole(CreateUserWizard1.UserName, "ONG");
+        }
+        catch (System.Configuration.Provider.ProviderException) {
+            Debug.WriteLine("am prins exceptia");
+        }
+    }
+
 }
