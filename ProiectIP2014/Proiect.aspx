cb97fd9cb0bdbceacb31a7740b125f4597b80ea9
mishu-mishu <elena.posea@gmail.com>
2014-04-02 23:30:32 +0300

    pot adauga comentarii si sunt reactualizare in timp real #7

--- ProiectIP2014/Proiect.aspx
+++ ProiectIP2014/Proiect.aspx
@@ -1,38 +1,49 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Proiect.aspx.cs" Inherits="Proiect" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-     <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString='<%$ ConnectionStrings:MySqlConnection %>' SelectCommand="SELECT Proiecte.id_proiect, Proiecte.cod_user, CONVERT (Date, Proiecte.data_inceput) AS data_i, CONVERT (Date, Proiecte.data_sfarsit) AS data_sfarsit, Proiecte.nume, Proiecte.descriere, Proiecte.continut, Proiecte.domeniu, aspnet_Users.UserName FROM Proiecte INNER JOIN aspnet_Users ON Proiecte.cod_user = aspnet_Users.UserId"></asp:SqlDataSource>
-    <div class="container" style=" max-width: 1000px;">
-    <div class="container" style="background-color: ghostwhite; max-width: 1000px;">
-        <asp:Repeater ID="Repeater1" DataSourceID="SqlDataSource1" runat="server">
-            <ItemTemplate>
-                <h2><%# DataBinder.Eval(Container.DataItem,"nume") %></h2>
-                <p class="text-danger"><%# DataBinder.Eval(Container.DataItem,"data_i") %> -- <%# DataBinder.Eval(Container.DataItem,"data_sfarsit") %></p>
-                <p><%# DataBinder.Eval(Container.DataItem,"descriere") %> </p>
-                p><%# DataBinder.Eval(Container.DataItem,"continut") %> </p>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString='<%$ ConnectionStrings:MySqlConnection %>' SelectCommand="SELECT Proiecte.id_proiect, Proiecte.cod_user, CONVERT (Date, Proiecte.data_inceput) AS data_i, CONVERT (Date, Proiecte.data_sfarsit) AS data_sfarsit, Proiecte.nume, Proiecte.descriere, Proiecte.continut, Proiecte.domeniu, aspnet_Users.UserName FROM Proiecte INNER JOIN aspnet_Users ON Proiecte.cod_user = aspnet_Users.UserId"></asp:SqlDataSource>
+    <asp:SqlDataSource ID="SqlDataSourceListaComentarii" runat="server" ConnectionString='<%$ ConnectionStrings:MySqlConnection %>' ></asp:SqlDataSource>
+    <div class="container" style="max-width: 1000px;">
+        <div class="container" style="background-color: ghostwhite; max-width: 1000px;">
+            <asp:Repeater ID="Repeater1" DataSourceID="SqlDataSource1" runat="server">
+                <ItemTemplate>
+                    <h2><%# DataBinder.Eval(Container.DataItem,"nume") %></h2>
+                    <p class="text-danger"><%# DataBinder.Eval(Container.DataItem,"data_i") %> -- <%# DataBinder.Eval(Container.DataItem,"data_sfarsit") %></p>
+                    <p><%# DataBinder.Eval(Container.DataItem,"descriere") %> </p>
+                    p><%# DataBinder.Eval(Container.DataItem,"continut") %></p>
                 
-             </ItemTemplate>
-         </asp:Repeater> 
-       
-        <asp:LoginView ID="LoginView1" runat="server">
-            <LoggedInTemplate>
-            
-                 <hr />
-                 <asp:TextBox ID="TextBox1" runat="server" Width="50%" Height="50px" TextMode="MultiLine"></asp:TextBox><br />
-                 <asp:Button CssClass="btn-default" ID="AddCommentButton"  runat="server" Text="Adauga un comentariu" />
-                 <hr />
-                    
-            </LoggedInTemplate>
-          </asp:LoginView>
-        
-        <asp:Label ID="Raspuns" runat="server" Text=" "></asp:Label> 
-    </div>
-    
-         <footer>
-        <p>&copy; Company 2014</p>
-      </footer>
+                </ItemTemplate>
+            </asp:Repeater>
+
+            <asp:LoginView ID="LoginViewComentarii" runat="server">
+                <LoggedInTemplate>
+
+                    <hr />
+                    <asp:TextBox ID="TextBoxContinut" runat="server" Width="50%" Height="50px" TextMode="MultiLine"></asp:TextBox><br />
+                    <asp:Button CssClass="btn-default" ID="AddCommentButton" runat="server" Text="Adauga un comentariu" OnClick="adaugaComentariu" />
+                    <hr />
+                    <!-- lista comentariilor deja postate -->
+                    <asp:Repeater ID="RepeaterComentarii" runat="server" DataSourceID="SqlDataSourceListaComentarii">
+                        <ItemTemplate>
+                            <%# DataBinder.Eval(Container.DataItem,"continut")  %>
+                            <br />
+                            <%# DataBinder.Eval(Container.DataItem,"data")  %>
+                            <br />
+                            by <%# DataBinder.Eval(Container.DataItem,"cod_user")  %>
+                            <br />
+
+                        </ItemTemplate>
+                    </asp:Repeater>
+                </LoggedInTemplate>
+            </asp:LoginView>
+            <asp:Label ID="Raspuns" runat="server" Text=" "></asp:Label>
+        </div>
+
+        <footer>
+            <p>&copy; Company 2014</p>
+        </footer>
     </div>
 </asp:Content>
 
--- ProiectIP2014/Proiect.aspx.cs
+++ ProiectIP2014/Proiect.aspx.cs
@@ -1,20 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Proiect : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        string id_proiect = Request.Params["id_proiect"];
+public partial class Proiect : System.Web.UI.Page {
+    private static string id_proiect;
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!Page.IsPostBack) {
+            id_proiect = Request.Params["id_proiect"];
+            if (id_proiect != null) {
+                SqlDataSource1.SelectCommand = "SELECT Proiecte.id_proiect, Proiecte.cod_user, CONVERT (Date, Proiecte.data_inceput) AS data_i, CONVERT (Date, Proiecte.data_sfarsit) AS data_sfarsit, Proiecte.nume, Proiecte.descriere, Proiecte.continut, Proiecte.domeniu, aspnet_Users.UserName FROM Proiecte INNER JOIN aspnet_Users ON Proiecte.cod_user = aspnet_Users.UserId where id_proiect = " + Server.UrlDecode(id_proiect) + ";";
 
-        if (id_proiect != null)
-        {
-            SqlDataSource1.SelectCommand = "SELECT Proiecte.id_proiect, Proiecte.cod_user, CONVERT (Date, Proiecte.data_inceput) AS data_i, CONVERT (Date, Proiecte.data_sfarsit) AS data_sfarsit, Proiecte.nume, Proiecte.descriere, Proiecte.continut, Proiecte.domeniu, aspnet_Users.UserName FROM Proiecte INNER JOIN aspnet_Users ON Proiecte.cod_user = aspnet_Users.UserId where id_proiect = " + Server.UrlDecode(id_proiect) + ";";
-           
+                // daca sunt logat
+                if (HttpContext.Current.User.Identity.IsAuthenticated) {
+                    SqlDataSourceListaComentarii.SelectCommand = "SELECT * FROM [Comentarii_Proiect] WHERE cod_proiect = " + id_proiect;
+                    SqlDataSourceListaComentarii.DataBind();
+                    string eu = Membership.GetUser(Profile.UserName).ProviderUserKey.ToString();
+                    if (eu_am_propus_proiectul(eu, id_proiect)) {
+                        // scriu cu o culoare mai tare, eu am propus proiectul
+                        Raspuns.Text = "eu l-am propus";
+                    }
+                    else {
+                        // daca sunt colaborator acceptat/sunt in bd 
+                        if (sunt_colaborator(eu, id_proiect)) {
+                            // scriu cu o anumita culoare, de colaborator
+                            Raspuns.Text = "eu sunt colaborator";
+                        }
+                        else LoginViewComentarii.Visible = false;
+                        // Raspuns.Text = "eu sunt " + eu;
+                    }
+                }
+                else {
+                    LoginViewComentarii.Visible = false;
+                }
+            }
         }
+
+    }
+
+    private bool eu_am_propus_proiectul(string eu, string id_proiect) {
+        SqlConnection connection = new SqlConnection();
+        connection.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\ASPNETDB.MDF;Integrated Security=True;User Instance=True";
+        connection.Open();
+        // vreau sa stiu id-ul celui care a propus proiectul
+        SqlCommand getIDCommand = new SqlCommand("SELECT cod_user FROM [Proiecte] WHERE id_proiect = " + id_proiect, connection);
+        SqlDataReader dr = getIDCommand.ExecuteReader();
+
+        if (dr.Read()) { // daca exista proiect cu id-ul dat
+            //Raspuns.Text = dr[0].ToString() + " a propus proiectul";
+            if (dr[0].ToString() == eu) return true;
+        }
+        // Debug.Write(dr[0].ToString() + " a propus proiectul");
+        dr.Close();
+        connection.Close();
+        return false;
+
+    }
+
+    private bool sunt_colaborator(string eu, string id_proiect) {
+        try {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\ASPNETDB.mdf;Integrated Security=True;User Instance=True";
+            connection.Open();
+            // vreau lista cu id-urile tututor colaboratorilor la acest proiect
+            // SqlCommand getListCommand = new SqlCommand("SELECT cod_user FROM [User_are_Colaboratori] WHERE cod_proiect = " + id_proiect, connection);
+            SqlCommand getListCommand = new SqlCommand("SELECT * FROM [User_are_Colaboratori]", connection);
+            SqlDataReader dr = getListCommand.ExecuteReader();
+            while (dr.Read()) { // daca mai am colaboratori
+                // Debug.Write(dr[0].ToString() + " e colaborator\n");
+                //Raspuns.Text += "<br /> " + dr[0].ToString();
+                if (dr[0].ToString() == eu) return true;
+            }
+            dr.Close();
+            connection.Close();
+            // Debug.Write("id_proiect = " + id_proiect);
+        }
+        catch (Exception ex) {
+            Debug.Write("am prins " + ex);
+        }
+        return false;
+    }
+
+    protected void adaugaComentariu(object sender, EventArgs e) {
+        SqlConnection connection = new SqlConnection();
+        connection.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\ASPNETDB.MDF;Integrated Security=True;User Instance=True";
+        connection.Open();
+        String q;
+        q = "INSERT INTO [Comentarii_Proiect] ([cod_user], [cod_proiect], [continut], [data]) VALUES (@cod_user, @cod_proiect, @continut, @data);";
+        SqlCommand commandComentariu = new SqlCommand(q, connection);
+        //if (HttpContext.Current.User.Identity.IsAuthenticated) {
+        commandComentariu.Parameters.AddWithValue("cod_user", Membership.GetUser(Profile.UserName).ProviderUserKey);
+        //}
+        commandComentariu.Parameters.AddWithValue("cod_proiect", id_proiect);
+        string continut = (LoginViewComentarii.FindControl("TextBoxContinut") as TextBox).Text;
+
+        commandComentariu.Parameters.AddWithValue("continut", continut);
+        commandComentariu.Parameters.AddWithValue("data", System.DateTime.Now);
+
+        commandComentariu.ExecuteNonQuery();
+
+        connection.Close();
+        //Raspuns.Text = "Articolul a fost inserat cu succes! ";
+        //Raspuns.CssClass = "ok";
+        SqlDataSourceListaComentarii.SelectCommand = "SELECT * FROM [Comentarii_Proiect] WHERE cod_proiect = " + id_proiect;
+        SqlDataSourceListaComentarii.DataBind();
+        (LoginViewComentarii.FindControl("RepeaterComentarii") as Repeater).DataBind();
+
+
     }
 }
